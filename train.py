10a0ff70e771b353cfb62a22ac0820d8695f78af
tomg hp envy <tomgray291@gmail.com>
2026-07-19 15:09:56 +0100

    Delete joblib model and add MLFlow logic

--- train.py
+++ train.py
@@ -1,8 +1,9 @@
+import mlflow
+import mlflow.sklearn
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-import joblib
 
 def main():
     # Load in the data
@@ -13,20 +14,27 @@ def main():
     # Split into test and train sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) 
 
-    # Train a random forest 
-    print("Training model...")
-    model = RandomForestClassifier(n_estimators=100, max_depth=3, random_state=42)
-    model.fit(X_train, y_train)
+    # Hyperparameters
+    n_estimators = 100
+    max_depth = 3
 
-    # Evaluate the model
-    predictions = model.predict(X_test)
-    accuracy = accuracy_score(y_test, predictions)
-    print(f"Model Accuracy: {accuracy:.2f}")
+    with mlflow.start_run():
+        # Train a random forest 
+        print("Training model...")
+        model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
+        model.fit(X_train, y_train)
 
-    # Save the model locally
-    print("Saving model to model.joblib...")
-    joblib.dump(model, 'model.joblib')
-    print("Done!")
+        # Evaluate the model
+        predictions = model.predict(X_test)
+        accuracy = accuracy_score(y_test, predictions)
+        print(f"Model Accuracy: {accuracy:.2f}")
+
+        # Experiment tracking with MLFlow
+        mlflow.log_param("n_estimators", n_estimators)
+        mlflow.log_param("max_depth", max_depth)
+        mlflow.log_metric("accuracy", accuracy)
+        mlflow.sklearn.log_model(model, "random-forest-model")
+        print("Experiment logged with MLFlow")
 
 if __name__ == "__main__":
     main()
