f80c008e7a752eadbe42b82efa5ef9281c32190a
tomg hp envy <tomgray291@gmail.com>
2026-07-19 16:02:45 +0100

    Fixed MLFlow issues

--- train.py
+++ train.py
@@ -15,8 +15,8 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) 
 
     # Hyperparameters
-    n_estimators = 100
-    max_depth = 3
+    n_estimators = 10
+    max_depth = 2
 
     with mlflow.start_run():
         # Train a random forest 
