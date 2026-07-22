b47e0659b6bc76613bf4e8a5974c5daf0e24c536
tomg hp envy <tomgray291@gmail.com>
2026-07-22 21:45:31 +0100

    Add continuous ml report function

--- train.py
+++ train.py
@@ -3,7 +3,12 @@ import mlflow.sklearn
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
 
 def main():
     # Load in the data
@@ -35,6 +40,22 @@ def main():
         mlflow.log_metric("accuracy", accuracy)
         mlflow.sklearn.log_model(model, "random-forest-model")
         print("Experiment logged with MLFlow")
+
+        # Generate confusion matrix 
+        cm = confusion_matrix(y_test, predictions)
+        plt.figure(figsize=(6,6))
+        sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=iris.target_names, yticklabels=iris.target_names )
+        plt.ylabel("Actual")
+        plt.xlabel("Predicted")
+        plt.title("Confusion Matrix")
+        plt.savefig("confusion_matrix.png")
+
+        # Write markdown file
+        with open("metrics.md", "w") as outfile:
+            outfile.write("## Model Evaluation\n")
+            outfile.write(f"**Accuracy:** {accuracy:.2f}\n\n")
+            outfile.write("### Confusion Matrix\n")
+            outfile.write("![Confusion Matrix](./confusion_matrix.png)\n")
 
 if __name__ == "__main__":
     main()
